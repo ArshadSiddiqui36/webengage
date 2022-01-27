b0016eeaeaee7970e3d3dc707cd72e05583dfa1d
shivamp1998 <shivampandey98@gmail.com>
2022-01-27 13:30:49 +0530

    added authentication

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,15 +13,51 @@ const User = require('../models/user')
      username: req.body.username,
      password: req.body.password
    });
+  User.addUser(newUser, (err,user) => {
+    if(err) {
+      console.log(err);
+      res.json({success: false, msg: 'Failed to Register User'});
+    }else{
+      res.json({success: true, msg: 'User Registered!'})
+    }
+  });
+
 
  });
 //authenticate route
-router.get('/authenticate',(req,res,next)=>{
-  res.send('authenticate done');
-})
+router.post('/authenticate',(req,res,next)=> {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username,(err,user)=> {
+    if(err) throw err;
+    if(username==="" || password==="") return res.json({success: false,msg: "All fields are required"});
+    if(!user) return res.json({success: false,msg: 'User not found'});
+    User.comparePassword(password, user.password, (err, isMatch)=> {
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign({data:user},"secret", {
+          expiresIn: 604800,
+        });
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        });
+      }else {
+        return res.json({success: false, msg: "wrong Password"});
+      }
+    })
+  })
+});
 //profile route
-router.get('/profile', (req,res,next)=>{
-  res.send('profile');
-})
+router.get('/profile',passport.authenticate('jwt',{session: false}),(req,res,next)=>{
+  res.json({user: req.user})
+});
 
 module.exports = router;
